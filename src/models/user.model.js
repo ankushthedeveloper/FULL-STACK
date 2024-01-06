671576dd11b4133ff9427a02c23994f46820566d
Ankush <ankushbokra@gmail.com>
2024-01-06 11:39:17 +0530

    tokens updated

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -12,7 +12,7 @@ const userSchema = new schema(
             index: true
         },
 
-        Email: {
+        email: {
 
             type: String,
             required: true,
@@ -50,7 +50,7 @@ const userSchema = new schema(
         watchHistory: [
             {
                 type: Schema.Types.ObjectId,
-                ref: "Video"
+                ref: "Video "
 
 
             }
@@ -72,7 +72,52 @@ const userSchema = new schema(
         timestamps:true
     }
 )
+userSchema.pre("save", async function(next){
+   if (!this.isModified("password")) return next()
 
+   this.password=bcrypt.hash(this.password,10)
+   next()
+
+   
+})
+userSchema.methods.isPasswordCorrect= async function(password){
+   return await bcrypt.compare(password,this.password)
+}
+
+userSchema.methods.generateAccessToken=function(){
+   return jwt.sign(
+        {
+            _id:this._id,
+            email:this.email,
+            username:this.username,
+            fullname:this.fullName
+        },
+
+
+        process.env.ACCESS_TOKEN_SECRET,
+        {
+            expiresIn:process.env.ACCESS_TOKEN_EXPIRY
+
+        }
+    )
+}
+userSchema.methods.generateAccessToken=function(){
+    return jwt.sign(
+        {
+            _id:this._id,
+            email:this.email,
+            username:this.username,
+            fullname:this.fullName
+        },
+
+
+        process.env.REFRESH_TOKEN_SECRET,
+        {
+            expiresIn:process.env.REFRESH_TOKEN_EXPIRY
+
+        }
+    )
+}
 export const User=mongoose.model("User",userSchema)
 
 
